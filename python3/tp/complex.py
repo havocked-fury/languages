04c22d80e84118a1851d847c9c4178f57e287e4e
havocked-fury <raiantasfin@tutanota.com>
2020-05-05 05:58:57 +0600

    Checking complex numbers in python

---
+++ python3/tp/complex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Checking complex numbers in python
+
+a = 35+20j
+b = 21+10j
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# print(a//b) ERROR: Can't take floor on complex numbers
+
+print(a**b)
