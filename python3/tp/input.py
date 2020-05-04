660dfab439a0ef63f67cf137af4fa2f96b5c714a
havocked-fury <raiantasfin@tutanota.com>
2020-05-05 05:54:48 +0600

    Variable Basics

---
+++ python3/tp/input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# First input program
+
+input("\n\nKEY PRESS")
---
+++ python3/tp/var.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# variable intro
+
+varint = 468168678
+varfloat = 6.28
+varstring = "STRING VAR VALUE"
+varchar = 'C'
+
+print(varint)
+print(varfloat)
+print(varstring)
+print(varchar)
+
+# Assignment Variations
+
+a = b = c = 69
+
+print(a)
+print(b)
+print(c)
+
+a, b, c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
