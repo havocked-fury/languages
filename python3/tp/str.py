fc60cd181fe525f2db65bc0e7d5c0689d24981e5
havocked-fury <raiantasfin@tutanota.com>
2020-05-05 06:05:28 +0600

    Checking strings

---
+++ python3/tp/str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Intro to strings
+
+str = "0123456789"
+
+print(str,"\t plain string")
+print(str[4], "\t [4]")
+print(str[2:5], "\t [2:5]")
+print(str[2:], "\t [2:]")
+print(str*2, "\t *2")
+print(str+"10", "\t +10")
